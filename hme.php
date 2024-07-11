2546d42d5b3e7d748a8b60d80627ee88017d9ff6
ribnuu <armrifbro3a@gmail.com>
2024-07-11 19:04:01 +0530

    Create hme.php
    Creating hme.php

---
+++ hme.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Adoption System</title>
+    <link rel="stylesheet" href="styles1.css">
+</head>
+<body>
+    <div class="background-wrapper">
+        <header>
+            <div class="left-header">
+                <img src="pictures/pet-shop-silhouette-logo-illustration-free-vector__1_-removebg-preview.png" alt="Site Logo" class="site-logo">
+            </div>
+            <div class="middle-header">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">List Pet</a>
+                <a href="#">Find a Pet</a>
+            </div>
+            <div class="right-header">
+                <button class="auth-button">
+                    <img src="pictures/user-sign-white-icon-vector-15479703-removebg-preview.png" alt="Logo" class="auth-logo"> Login/Register
+                </button>
+            </div>
+        </header>
+        <main>
+            <h1 class="page-title">Adopt a Pet: Find Your New Friend</h1>
+            <div class="content">
+                <div class="vision">
+                    <h2>Vision</h2>
+                    <p>"To create loving homes for every pet, ensuring they find happiness and companionship."
+
+                    </p>
+                </div>
+                <div class="mission">
+                    <h2>Mission</h2>
+                    <p>"To connect prospective pet owners with their ideal companions, promoting responsible pet adoption and lifelong bonds."</p>
+                </div>
+            </div>
+        </main>
+    </div>
+    <footer>
+        <div class="footer-section">
+            <h3>About</h3>
+            <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Feedback</a></li>
+            </ul>
+        </div>
+        <div class="footer-section">
+            <h3>List a Pet</h3>
+        </div>
+        <div class="footer-section">
+            <h3>Find a Pet</h3>
+        </div>
+        <div class="footer-section">
+            <h3>My Account</h3>
+            <ul>
+                <li><a href="#">Sign In</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
